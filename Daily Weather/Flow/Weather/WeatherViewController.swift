4f9aa440a4076e93a303b7ea222efbc9895d5f74
kwazzar <fugitiwe@gmail.com>
2022-07-13 23:09:17 +0300

    Flow

---
+++ Daily Weather/Flow/Weather/WeatherViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Daily Weather
+//
+//  Created by Quasar on 13.07.2022.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    @IBOutlet weak var weatherIconImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var feelsLikeTemperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    
+    @IBOutlet weak var activityIndicatorView: UIActivityIndicatorView!
+    
+            override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
+
